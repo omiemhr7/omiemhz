84e5e83bf5e0fce22fda0a128b33d46adcc70ec6
omiemhr7 <omiemh.r7@gmail.com>
2026-09-26 02:20:23 +0300

    Fix auth schema corruption and user creation

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -128,4 +128,4 @@ export const ALLOWED_FILE_TYPES = [
   'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
 ];
 
-export const ADMIN_EMAIL = 'Omiemh.r7@gmail.com';
+export const ADMIN_EMAIL = 'omiemh.r7@gmail.com';
